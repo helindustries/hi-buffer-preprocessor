d867094409d737f7cabd4918798509b86ee25cf3
Paul Schulze <pschulze@helindustries.com>
2025-04-02 21:00:31 +0200

    Make sure there is no error printed to the console when there are no generated buffer files

--- BufferPreprocessor.mk
+++ BufferPreprocessor.mk
@@ -41,7 +41,7 @@ HEADER_BUFFER_FILES := $(HEADER_BUFFER_FILES:%.h=%.Data.h)
 
 # Need to add these to the build files, so they actually get recognized as dependencies
 HEADERS += $(HEADER_BUFFER_FILES) $(CPP_BUFFER_FILES)
-$(foreach path,$(HEADER_BUFFER_FILES) $(CPP_BUFFER_FILES),$(shell grep "// <Incomplete>" $(abspath $(path)) > /dev/null && rm -f $(path)))
+$(foreach path,$(HEADER_BUFFER_FILES) $(CPP_BUFFER_FILES),$(shell grep "// <Incomplete>" $(abspath $(path)) >/dev/null 2>&1 && rm -f $(path)))
 
 buffers: $(CPP_BUFFER_FILES) $(HEADER_BUFFER_FILES) $(BUFFER_PROCESSOR_PATH) | silent
 	@
