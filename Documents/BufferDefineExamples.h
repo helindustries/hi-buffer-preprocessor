10b49106dea6621baa9125d59dd255fa7d3e8ccd
Paul Schulze <pschulze@helindustries.com>
2025-05-04 21:34:15 +0200

    Update copyright of buffer utility

--- Documents/BufferDefineExamples.h
+++ Documents/BufferDefineExamples.h
@@ -1,8 +1,4 @@
-//  Copyright 2023 Hel Industries, all rights reserved.
-//
-//  For licensing terms, Please find the licensing terms in the closest
-//  LICENSE.txt in this repository file going up the directory tree.
-//
+// Copyright 2023-2025 $author, All rights reserved.
 
 #pragma once
 
